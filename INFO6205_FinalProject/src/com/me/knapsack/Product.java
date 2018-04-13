95efa0f433daa2860a1e959c326e55d6efa07049
Riddhi <kamlesh.r@husky.neu.edu>
2018-04-13 02:44:43 -0400

    Population,Product,values files

---
+++ INFO6205_FinalProject/src/com/me/knapsack/Product.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.me.knapsack;
+
+/**
+ *
+ * @author HP
+ */
+public class Product {
+    private int weight;
+    private int price;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int volume) {
+        this.weight = volume;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    
+}
---
+++ INFO6205_FinalProject/src/com/me/knapsack/Values.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.me.knapsack;
+
+/**
+ *
+ * @author HP
+ */
+public class Values {
+    
+    public static int totalItems=10;
+    public static int initialpop=100;
+    public static int capacity=25;
+}
