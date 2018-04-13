c8ab0d729f5860be95d88d383e8bb912855384ea
Riddhi <kamlesh.r@husky.neu.edu>
2018-04-13 17:51:30 -0400

    adding cross-over function to create children

--- INFO6205_FinalProject/src/com/me/knapsack/Main.java
+++ INFO6205_FinalProject/src/com/me/knapsack/Main.java
@@ -18,73 +18,102 @@ import java.util.Random;
  * @author HP
  */
 public class Main {
-    public static List<Product> productList=new ArrayList<>();
+
+    public static List<Product> productList = new ArrayList<>();
     public static Population pop;
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         createProducts();
         System.out.println(productList.size() + " Products have been created as:");
         System.out.println("Product No.    Price      Weight");
         for (int i = 0; i < productList.size(); i++) {
             System.out.println(i + "            " + productList.get(i).getPrice() + "           " + productList.get(i).getWeight());
         }
-        
+
         /////Initialize population
-        pop=new Population(initialpop);
-        
-        tracker();
-        for(int i=1;i<2;i++){
+        pop = new Population(initialpop);
+
+        tracker(0);
+        for (int i = 1; i < 15; i++) {
             selection();
-            tracker();
-        }
-        
-        
-    } 
-        
-//    initially generates products
-    public static void createProducts(){
-        Random rand=new Random();  
-        for(int i=0;i<totalItems;i++){
-            Product p=new Product();
-            p.setWeight(rand.nextInt(10)+1);
-            p.setPrice(rand.nextInt(10)+1);
-            productList.add(p);
-        }
-    }
-    
-    public static void tracker(){
-        System.out.println(initialpop + " Initial chromosomes have been generated as:");
-        System.out.println("Chromosome      Weight      Value       Fitness");
-        for(int i=0;i<pop.getIndividuals().size();i++){
-            for(int g:pop.getIndividuals().get(i).getGenes()){
-                System.out.print(g);
-                }
-            System.out.println("        " + pop.getIndividuals().get(i).getWeight()+ "          " + pop.getIndividuals().get(i).getPrice()
-            + "           " + pop.getIndividuals().get(i).getFitness());
-    
+            evolvePopulation();
+            tracker(i);
         }
 
     }
-    
-    public static void selection(){
-        Comparator<Individual> sort=(Individual ind1, Individual ind2)->{
-            int cmp1=ind1.getFitness();
-            int cmp2=ind2.getFitness();
-            if(cmp2>cmp1) return 1;
-            
-            else if(cmp2<cmp1) return -1;
-            
-            else return 0;
+
+//    initially generates products
+    public static void createProducts() {
+        Random rand = new Random();
+        for (int i = 0; i < totalItems; i++) {
+            Product p = new Product();
+            p.setWeight(rand.nextInt(10) + 1);
+            p.setPrice(rand.nextInt(10) + 1);
+            productList.add(p);
+        }
+    }
+
+    public static void tracker(int generationCount) {
+        System.out.println("For generation: " + generationCount + " chromosomes have been generated as:");
+        System.out.println("Chromosome      Weight      Value       Fitness");
+        for (int i = 0; i < pop.getIndividuals().size(); i++) {
+            for (int g : pop.getIndividuals().get(i).getGenes()) {
+                System.out.print(g);
+            }
+            System.out.println("        " + pop.getIndividuals().get(i).getWeight() + "          " + pop.getIndividuals().get(i).getPrice()
+                    + "           " + pop.getIndividuals().get(i).getFitness());
+
+        }
+
+    }
+
+    public static void selection() {
+        Comparator<Individual> sort = (Individual ind1, Individual ind2) -> {
+            int cmp1 = ind1.getFitness();
+            int cmp2 = ind2.getFitness();
+            if (cmp2 > cmp1) {
+                return 1;
+            } else if (cmp2 < cmp1) {
+                return -1;
+            } else {
+                return 0;
+            }
         };
-        
-        Queue<Individual> q=new PriorityQueue<>(sort);
-        List<Individual> individuals=pop.getIndividuals();
-        for(int i=0;i<individuals.size();i++) q.add(individuals.get(i));
-        int halfpop=individuals.size()/2;
+
+        Queue<Individual> q = new PriorityQueue<>(sort);
+        List<Individual> individuals = pop.getIndividuals();
+        for (int i = 0; i < individuals.size(); i++) {
+            q.add(individuals.get(i));
+        }
+        int halfpop = individuals.size() / 2;
         individuals.clear();
-        for(int i=0;i<halfpop;i++){
+        for (int i = 0; i < halfpop; i++) {
             individuals.add(q.poll());
         }
-        
+    }
+
+    public static void evolvePopulation() {
+        List<Individual> individuals = pop.getIndividuals();
+        for (int i = 0; i < individuals.size() - 1; i += 2) {
+            Individual i1 = individuals.get(i);
+            Individual i2 = individuals.get(i + 1);
+            crossover(i1, i2, individuals);
+        }
+    }
+
+    public static void crossover(Individual i1, Individual i2, List<Individual> individuals) {
+        Random rand = new Random();
+        Individual child = new Individual();
+        for (int i = 0; i < individuals.get(i).getGenes().size(); i++) {
+            int r = rand.nextInt(2);
+            if (r == 0) {
+                child.getGenes().add(i2.getGenes().get(i));   //for each gene from parents'
+            } else {
+                child.getGenes().add(i1.getGenes().get(i));
+            }
+        }
+        child.calculateFitness();
+        pop.getIndividuals().add(child);
     }
 }
