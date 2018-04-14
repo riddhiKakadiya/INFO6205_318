f2a227d31304190ac966f7ec22bc07db198cffef
Hiren <shah.hi@husky.neu.edu>
2018-04-14 19:53:24 -0400

    Adding selection and evlove test class

--- INFO6205_FinalProj/src/test/java/com/me/knapsack/KnapsackTest.java
+++ INFO6205_FinalProj/src/test/java/com/me/knapsack/KnapsackTest.java
@@ -1,6 +1,9 @@
 package com.me.knapsack;
 
+import static com.me.knapsack.Main.evolvePopulation;
 import static com.me.knapsack.Main.productList;
+import static com.me.knapsack.Main.selection;
+import static com.me.knapsack.Values.initialpop;
 import static com.me.knapsack.Values.totalItems;
 import java.util.Random;
 import org.junit.Assert;
@@ -25,13 +28,20 @@ public class KnapsackTest {
             productList.add(p);
         }
 
-        Population pop = new Population(totalItems);
+        Population pop = new Population(initialpop);
         Assert.assertFalse(pop.individuals.get(1).getGenes().equals(pop.individuals.get(0).getGenes()));
     }
 
     @Test
     public void generateProduct() {
         //test whether sack size is 20
+
+        Assert.assertEquals(productList.size(), 20);
+    }
+
+    @Test
+    public void selectionTest() {
+        
         Random rand = new Random();
         for (int i = 0; i < totalItems; i++) {
             Product p = new Product();
@@ -42,11 +52,32 @@ public class KnapsackTest {
             productList.add(p);
         }
 
-        Assert.assertEquals(productList.size(), 20);
+        Population pop = new Population(initialpop);
+        selection();
+        pop.calcBestSolution();
+        Assert.assertTrue(pop.individuals.get(0).getFitness()>=pop.individuals.get(1).getFitness());
+        Assert.assertEquals(pop.individuals.get(0).getFitness(),pop.getFittest().getFitness());
     }
-
+    
     @Test
-    public void selectionTest() {
+    public void evolveTest(){
+        Random rand = new Random();
+        for (int i = 0; i < totalItems; i++) {
+            Product p = new Product();
 
+            p.setWeight(rand.nextInt(10) + 1);
+            p.setPrice(rand.nextInt(10) + 1);
+
+            productList.add(p);
+        }
+        Population pop = new Population(initialpop);
+        selection();
+        int beforeCrossover=pop.getIndividuals().size();
+        evolvePopulation();
+        int afterCrossover=pop.getIndividuals().size();
+        
+        Assert.assertFalse(beforeCrossover==afterCrossover);
+        Assert.assertEquals(afterCrossover,initialpop);
     }
+    
 }
